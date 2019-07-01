817d37762e3ee3f4a4b2340891f4f3dadb00328a
Takumi Muraishi <mrs1669.develop@gmail.com>
2019-07-01 11:15:02 +0900

    nwew

---
+++ amahako_API/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  amahako_API
+//
+//  Created by Takumi Muraishi on 2019/06/17.
+//  Copyright © 2019 Takumi Muraishi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
