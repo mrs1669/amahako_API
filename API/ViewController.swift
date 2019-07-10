7e854c778cd5a1c72604494fbacf4d84ac5bff8d
Takumi Muraishi <mrs1669.develop@gmail.com>
2019-07-10 14:38:46 +0900

    セルの中をタップしたときの挙動を作りたかった...orz

--- API/ViewController.swift
+++ API/ViewController.swift
@@ -10,13 +10,14 @@ import UIKit
 
 struct Article: Codable {
     
+    var studentNumber: String
     var name: String
-    var owner: Owner
+    //var owner: Owner
     
-    struct Owner: Codable{
-        var login: String
-        var avatar_url: String
-    }
+   // struct Owner: Codable{
+   //     var login: String
+   //     var avatar_url: String
+   // }
 }
 
 
@@ -26,7 +27,7 @@ struct Git {
     
     static func fetchArticle(completion: @escaping ([Article]) -> Swift.Void) {
         
-        let url = "https://api.github.com/repositories?since=192290297"
+        let url = "http://mrs1669.html.xdomain.jp/test.json"
         
         guard let urlComponents = URLComponents(string: url) else {
             return
@@ -74,6 +75,7 @@ class ViewController: UIViewController{
     }
 }
 
+/*
 extension UIImageView {
     
     func downloadImage(from url: String) {
@@ -93,23 +95,55 @@ extension UIImageView {
         }
         task.resume()
     }
-    
 }
+*/
 
-extension ViewController: UITableViewDataSource {
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
         let article = articles[indexPath.row]
+        cell.detailTextLabel?.text = article.studentNumber
         cell.textLabel?.text = article.name
-        cell.detailTextLabel?.text = article.owner.login
         cell.accessoryType         = UITableViewCell.AccessoryType.detailDisclosureButton
-        cell.imageView?.image      = UIImage(named: "wai.jpeg")
+        //cell.imageView?.image      = UIImage(named: "wai.jpeg")
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return articles.count
+    }
+    
+    // アクセサリーボタンをタップした時の処理
+    func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        // 選択された駅名
+        //let article = articles[indexPath.row]
+        
+        // アラートを作成
+        let alert = UIAlertController(title: "もしかして", message: "駅に興味があるんですか！？", preferredStyle: .alert)
+        let buttonYes = UIAlertAction(title: "めっちゃあります！", style: .default, handler: nil)
+        let buttonNo = UIAlertAction(title: "さっぱりないです", style: .destructive, handler: nil)
+        alert.addAction(buttonYes)
+        alert.addAction(buttonNo)
+        
+        // アラートを表示
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // セルを非選択状態にする
+        //table.deselectRow(at: indexPath, animated: true)
+        
+        // 選択された駅名
+        //let station = articles[indexPath.row]
+        
+        // アラートを作成
+        let alert = UIAlertController(title: "駅", message: "千歳線の駅です。", preferredStyle: .alert)
+        let buttonOk = UIAlertAction(title: "了解", style: .default, handler: nil)
+        alert.addAction(buttonOk)
+        
+        // アラートを表示
+        present(alert, animated: true, completion: nil)
     }
 }
